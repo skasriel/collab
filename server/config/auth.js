a4c8b50af3ef610ced5f853d9bcc6901c41211b2
Stephane Kasriel <skasriel@elance-odesk.com>
2014-05-15 22:55:48 -0700

    config for heroku

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,13 +3,13 @@ module.exports = {
 	'oDeskAuth' : {
 		'clientID' 		: '3f448b92c4aaf8918c0106bd164a1656',
 		'clientSecret' 	: 'e6a71b4f05467054',
-		'callbackURL' 	: 'http://localhost:3000/auth/odesk/callback'
+		'callbackURL' 	: process.env.NODE_ENV=='staging' ? 'http://fast-caverns-8687.herokuapp.com/auth/odesk/callback' : 'http://localhost:3000/auth/odesk/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '626791004779-f61f22vjoc8shkliqb79l1aa463a0okt.apps.googleusercontent.com',
 		'clientSecret' 	: '2kTwdDf27vKwvz21HUJOOlHJ',
-		'callbackURL' 	: 'http://localhost:3000/auth/google/callback'
+		'callbackURL' 	: process.env.NODE_ENV=='staging' ? 'http://fast-caverns-8687.herokuapp.com/auth/google/callback' || 'http://localhost:3000/auth/google/callback'
 	}
 
 };
